9a2a10d3d3640e1e44dc3a5331ea97f8d40c459a
Yaroslav <6715414@gmail.com>
2023-02-12 12:05:15 +0200

    added declare for icons

--- app.d.ts
+++ app.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="nativewind/types" />
 declare module '*.jpg';
 declare module '*.png';
+declare module 'react-native-vector-icons/Entypo';
+declare module 'react-native-vector-icons/FontAwesome';
