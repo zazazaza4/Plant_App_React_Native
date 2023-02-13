c595e5b2227ad4ee7deac578ca691460073c1fab
Yaroslav <6715414@gmail.com>
2023-02-13 22:35:58 +0200

    added Item screen

--- App.tsx
+++ App.tsx
@@ -9,8 +9,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import HomeScreen from './app/screens/HomeScreen';
-import Discover from './app/screens/Discover';
+import {Discover, HomeScreen, ItemScreen} from './app/screens';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,6 +23,7 @@ function App(): JSX.Element {
           }}>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Discover" component={Discover} />
+          <Stack.Screen name="ItemScreen" component={ItemScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
